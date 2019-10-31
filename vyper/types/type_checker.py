364b12e6e93ae0eb651e3b69b6b132f65bd1cb67
Charles Cooper <cooper.charles.m@gmail.com>
2019-10-31 22:54:13 +0100

    wip type checker

---
+++ vyper/types/type_checker.py
@@ -0,0 +1,8 @@
+# module for type checking
+# type checking currently happens all over the place, but try to
+# migrate type checking code here.
+def can_assign(lhs, rhs):
+    pass
+
+def infer_arithmetic(lhs, rhs):
+    pass
