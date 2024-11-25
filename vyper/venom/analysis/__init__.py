bbe434182141de452f9e312472283a9d90b6f101
Charles Cooper <cooper.charles.m@gmail.com>
2024-11-25 10:48:15 +0100

    wip - tarjan

--- vyper/venom/analysis/__init__.py
+++ vyper/venom/analysis/__init__.py
@@ -1,5 +1,6 @@
 from .analysis import IRAnalysesCache, IRAnalysis
 from .cfg import CFGAnalysis
+from .scc import SCCAnalysis
 from .dfg import DFGAnalysis
 from .dominators import DominatorTreeAnalysis
 from .equivalent_vars import VarEquivalenceAnalysis
--- vyper/venom/analysis/scc.py
+++ vyper/venom/analysis/scc.py
@@ -1,36 +1,51 @@
+from dataclasses import dataclass,field
 from vyper.utils import OrderedSet
 from vyper.venom.analysis import CFGAnalysis, IRAnalysis
-from typing import TYPE_CHECKING
 
-if TYPE_CHECKING:
-    from vyper.venom.basic_block import IRBasicBlock
+from vyper.venom.basicblock import IRBasicBlock
+
+@dataclass
+class SCC:
+    root: IRBasicBlock
+
+    basicblocks: OrderedSet[IRBasicBlock] #= field(default_factory=OrderedSet)
+
+    #next_sccs: OrderedSet["SCC"] #= field(default_factory = OrderedSet)
 
 class SCCAnalysis(IRAnalysis):
     """
     Strongly Connected Components Analysis
     """
     def analyze(self):
-        cfg = self.request_analysis(CFGAnalysis)
+        cfg = self.analyses_cache.request_analysis(CFGAnalysis)
 
         self._sccs = {}
+        self._lowlinks = {}
+        self._stack = []
+        self._dfs_order = 0
 
-        self._stack = OrderedSet()
-
-        self._analyze_r(self.function.entry)
+        for bb in self.function.get_basic_blocks():
+            if bb not in self._lowlinks:
+                self._analyze_r(bb)
 
     def _analyze_r(self, bb: "IRBasicBlock"):
-        if bb in self._sccs:
-            return
+        self._lowlinks[bb] = self._dfs_order
+        self._dfs_order += 1
+        self._stack.append(bb)
 
-        if bb not in self._stack:
-            self._stack.add(bb)
-            for next_bb in bb.cfg_out:
+        for next_bb in bb.cfg_out:
+            if next_bb not in self._lowlinks:
                 self._analyze_r(next_bb)
-        else:
-            # found a cycle.
+                self._lowlinks[bb] = min(self._lowlinks[bb], self._lowlinks[next_bb])
+            elif next_bb in self._stack:
+                self._lowlinks[bb] = min(self._lowlinks[bb], self._lowlinks[next_bb])
+
+        if self._lowlinks[bb] == bb:
             root = bb
+            scc = OrderedSet(bb, OrderedSet())
             while True:
-                bb = self._stack.pop()
-                self._sccs[bb] = root
+                bb = self.stack.pop()
+                scc.basicblocks.add(bb)
+                self.sccs[bb] = scc
                 if bb == root:
                     break
