1d2ae11b7d62616623051b33c5c4670f4bbea1b4
Charles Cooper <cooper.charles.m@gmail.com>
2025-04-26 15:16:46 +0200

    fix replacement

--- vyper/venom/passes/store_elimination.py
+++ vyper/venom/passes/store_elimination.py
@@ -1,4 +1,5 @@
-from vyper.venom.analysis import DFGAnalysis, LivenessAnalysis, DominatorTreeAnalysis
+from vyper.venom.analysis import DFGAnalysis, LivenessAnalysis, DominatorTreeAnalysis, VarDefinition
+from vyper.venom.check_venom import check_venom_fn
 from vyper.venom.basicblock import IRVariable
 from vyper.venom.passes.base_pass import InstUpdater, IRPass
 
@@ -24,7 +25,11 @@ class StoreElimination(IRPass):
                 continue
             self._process_store(inst, var, inst.operands[0])
 
+        for bb in self.function.get_basic_blocks():
+            bb.ensure_well_formed()
+
         self.analyses_cache.invalidate_analysis(LivenessAnalysis)
+        self.analyses_cache.invalidate_analysis(VarDefinition)
 
     def _process_store(self, inst, var: IRVariable, new_var: IRVariable):
         """
@@ -34,11 +39,19 @@ class StoreElimination(IRPass):
         uses = self.dfg.get_uses(var)
         for use_inst in list(uses):
             if use_inst.opcode == "phi":
-                new_src = self.dfg.get_producing_instruction(new_var)
-                valid_replacement = new_src is not None and self.dom.dominates(new_src.parent, use_inst.parent)
-                if not valid_replacement:
+                new_source = self.dfg.get_producing_instruction(new_var)
+                if new_source is None:
                     continue
+                # invalid replacement
+                if not self.dom.dominates(new_source.parent, inst.parent):
+                    continue
+
             self.updater.update_operands(use_inst, {var: new_var})
+
+            if use_inst.opcode == "phi":
+                ops = [var for _, var in use_inst.phi_operands]
+                if all(op == ops[0] for op in ops):
+                    self.updater.store(use_inst, ops[0])
 
         if len(uses) == 0:
             self.updater.remove(inst)
